c52335154eeb2f6face70c047625904ab65fef9f
TigerWenhao <72538010+TigerWenhao@users.noreply.github.com>
2021-02-28 00:43:07 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,5 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+#
+sed -i "s/OpenWrt /OpenWrt Tiger $(TZ=UTC-8 date "+%Y.%m.%d") /g" package/lean/default-settings/files/zzz-default-settings
